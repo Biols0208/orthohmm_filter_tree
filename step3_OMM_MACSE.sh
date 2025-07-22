55b919f7be9c47c2d60aebf1f003e03860c56bd6
Biols <50230939+Biols0208@users.noreply.github.com>
2025-07-22 22:58:11 +0800

    Create step3_OMM_MACSE.sh

---
+++ step3_OMM_MACSE.sh
@@ -0,0 +1,5 @@
+##
+for i in $(cat orthohmm_gene_count.txt.out1.deal1.group)
+do
+echo "singularity run omm_macse.v10.02.sif --in_seq_file $PWD/out/${i}/${i}.best.fa --out_dir $PWD/out/${i}/omm_macse --out_file_prefix ${i} --save_details --java_mem 10000m --min_percent_NT_at_ends 0.7" >> all_OMM_MACSE.sh
+done
