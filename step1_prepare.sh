cfe0f02e24a64d6871d4df128caa314f88c4f6c0
Biols <50230939+Biols0208@users.noreply.github.com>
2025-07-22 22:20:22 +0800

    Update step1_prepare.sh

--- step1_prepare.sh
+++ step1_prepare.sh
@@ -1,14 +1,18 @@
+## calculate
 python step1_deal_orthohmm_gene_count.py orthohmm_gene_count.txt orthohmm_gene_count.txt.out1
 
-##  StdDev<1 && Species_Present_Percent>60
-awk '$69<1 && $75>36' orthohmm_gene_count.txt.out1 > orthohmm_gene_count.txt.out1.deal1
+## StdDev<1 && Species_Present_Percent>90
+awk '$69<1 && $(NF-2)>90' orthohmm_gene_count.txt.out1 > orthohmm_gene_count.txt.out1.deal1
 awk '{print $1}' orthohmm_gene_count.txt.out1.deal1 | sed 's/://g' > orthohmm_gene_count.txt.out1.deal1.group
-mkdir cds
 
-cat orthohmm_gene_count.txt.out1.deal1.group | parallel --jobs 48 '
+## get cds sequence from all cds file
+[ -d cds ] || mkdir cds
+input=all_species_in_one.cds.fa
+thread=48
+cat orthohmm_gene_count.txt.out1.deal1.group | parallel --jobs ${thread} '
     grep -f <(echo {}) orthohmm_orthogroups.txt \
     | sed "s/ /\n/g" | sed "1d" \
-    | seqtk subseq all.deal.cds - > cds/{}.cds
+    | seqtk subseq ${input} - > cds/{}.cds
 '
 
 #for i in $(cat orthohmm_gene_count.txt.out1.deal1.group); do echo ${i} | grep -f - orthohmm_orthogroups.txt | sed 's/ /\n/g' | sed '1d' | seqtk subseq all.deal.cds - > cds/${i}.cds; done
