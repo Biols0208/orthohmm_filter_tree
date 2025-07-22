315eac5ca35b1137ebe4f81cb9d698da8f212147
Biols <50230939+Biols0208@users.noreply.github.com>
2025-07-22 23:10:37 +0800

    Update step4_FastTree.sh

--- step4_FastTree.sh
+++ step4_FastTree.sh
@@ -1,2 +1,11 @@
+##
+for i in $(cat orthohmm_gene_count.txt.out1.deal1.group)
+do
+cp constructing_gene_tree.sh $PWD/out/${i}/omm_macse
+done
 
-for i 
+##
+for i in $(cat orthohmm_gene_count.txt.out1.deal1.group)
+do
+cd $PWD/out/${i}/omm_macse && sh constructing_gene_tree.sh && cd ../../../
+done
