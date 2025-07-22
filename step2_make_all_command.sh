aeb903141f53a8e807ed951f05357b0de0a8e8a9
Biols <50230939+Biols0208@users.noreply.github.com>
2025-07-22 22:53:44 +0800

    Update step2_make_all_command.sh

--- step2_make_all_command.sh
+++ step2_make_all_command.sh
@@ -1,7 +1,8 @@
 ##
+mkdir out
 for i in $(cat orthohmm_gene_count.txt.out1.deal1.group)
 do 
-echo "sh get_best_from_family.sh $PWD/cds/${i}.cds -o ${i} -d $PWD/out_tiqu/${i} -t 8 --no-timestamp --force --log-level DEBUG " >> get_best.sh
+echo "sh get_best_from_family.sh $PWD/cds/${i}.cds -o ${i} -d $PWD/out/${i} -t 8 --no-timestamp --force --log-level DEBUG " >> get_best.sh
 done
 
 ##
