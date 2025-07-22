e0bccaf6a6016b9a787c4b26ecdda5fa9e62f1f1
Biols <50230939+Biols0208@users.noreply.github.com>
2025-07-22 22:34:52 +0800

    Create step2_deal_cds_new.sh

---
+++ step2_deal_cds_new.sh
@@ -0,0 +1,10 @@
+##
+for i in $(cat orthohmm_gene_count.txt.out1.deal1.group)
+do 
+echo "sh get_best_from_family.sh $PWD/cds/${i}.cds -o ${i} -d $PWD/out_tiqu/${i} -t 8 --no-timestamp --force --log-level DEBUG " >> get_best.sh
+done
+
+##
+parallel --jobs 48 bash -c {} < get_best.sh
+
+# Or split get_best.sh and run
